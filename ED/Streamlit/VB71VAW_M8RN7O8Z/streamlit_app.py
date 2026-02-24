8bc49a30bb405b6a61d1e649f7a97be347fd3a27
Sushant Kumar <sushant.kumar@snowflake.com>
2026-02-24 02:51:46 +0000

    Second Commit

--- ED/Streamlit/VB71VAW_M8RN7O8Z/streamlit_app.py
+++ ED/Streamlit/VB71VAW_M8RN7O8Z/streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
-st.title(f"New Demo Streamlit App {st.__version__}")
+st.title(f"New Demo Streamlit App_1")
 st.write(
   """Replace this example with your own code!
   **And if you're new to Streamlit,** check
